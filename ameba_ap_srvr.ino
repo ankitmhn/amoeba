a83355ded919e2e752e10d6fa69805bdff800f19
superliner <ankit.mohan@live.in>
2016-07-14 14:22:12 +0530

    Add files via upload
    Initial commit - DS1307 support in AP mode.

---
+++ ameba_ap_srvr.ino
@@ -0,0 +1,118 @@
+
+#include <HttpClient.h>
+
+#include <WiFi.h>
+
+char ssid[] = "ameba";  //Set the AP's SSID
+char pass[] = "amebaiot";     //Set the AP's password
+char channel[] = "1";         //Set the AP's channel
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+WiFiServer server(80);
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  //Init and set all Zones to LOW
+  for(int i = 1; i <- 4; i++){
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+
+  //Start the RTC assumption: RTC is initialized and is running
+  ds1307_init();
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+  String fv = WiFi.firmwareVersion();
+  if (fv != "1.1.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to start AP:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to start AP with SSID: ");
+    Serial.println(ssid);
+    status = WiFi.apbegin(ssid, pass, channel);
+    delay(10000);
+  }
+
+  //AP MODE already started:
+  Serial.println("AP mode already started");
+  Serial.println();
+  server.begin();
+  //printWifiData();
+  //printCurrentNet();
+}
+
+
+void loop() {
+
+  //Check time and set the zones
+  displayTime();
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int i = 1; i <= 4; i++){
+            client.print("Zone ");
+            client.print(i);
+            client.print(": ");
+            if(digitalRead(i) == HIGH) client.print("ON");
+            else client.print("OFF");
+            client.println("<br />");
+          }
+          /* Original for-loop here
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          */
+          client.println("</html>");
+          break;
+          }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
