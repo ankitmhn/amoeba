da27bb720a92e5b6819c02cf5f017be211847ec5
superliner <ankit.mohan@live.in>
2016-08-24 23:36:24 +0530

    Add files via upload

--- ds1307.ino
+++ ds1307.ino
@@ -1,6 +1,23 @@
 #include "Wire.h"
 #define DS3231_I2C_ADDRESS 0x68
 // Convert normal decimal numbers to binary coded decimal
+
+/*vod RTC_setup(){
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+}
+*/
 byte decToBcd(byte val)
 {
   return( (val/10*16) + (val%10) );
